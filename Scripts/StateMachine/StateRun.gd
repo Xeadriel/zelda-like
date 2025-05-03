df82bd4d1ff85ba349d3151e559d544e5ee8f5cf
Xeadriel <ogulcanasik@web.de>
2025-05-03 15:23:57 +0200

    removed unnecessary code in StateRun

--- Scripts/StateMachine/StateRun.gd
+++ Scripts/StateMachine/StateRun.gd
@@ -16,32 +16,27 @@ func process(_delta: float) -> void:
 	pass
 
 func physicsProcess(delta: float) -> void:
-	if Input.is_action_pressed("hit"):
+	if Input.is_action_just_pressed("hit"):
 		finished.emit("StateAttack")
 
-	if (Input.is_action_pressed("left") or
-		Input.is_action_pressed("right") or
-		Input.is_action_pressed("up") or
-		Input.is_action_pressed("down")
-		):
-		var direction :=  Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
-		if direction:
-			entity.velocity = direction.normalized() * SPEED
-
-			if direction.y < 0:
-				entity.direction = Direction.UP
-			elif direction.y > 0:
-				entity.direction = Direction.DOWN
-
-			# horizontal direction prioritized over vertical direction
-			if direction.x < 0:
-				entity.direction = Direction.LEFT
-			elif direction.x > 0:
-				entity.direction = Direction.RIGHT
-		else:
-			entity.velocity = entity.velocity.move_toward(Vector2.ZERO, SPEED)
+	var direction :=  Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	if direction:
+		entity.velocity = direction.normalized() * SPEED
+		setPlayerDirection(direction)
 	else:
 		finished.emit("StateIdle")
+
+func setPlayerDirection(direction : Vector2) -> void:
+	if direction.y < 0:
+		entity.direction = Direction.UP
+	elif direction.y > 0:
+		entity.direction = Direction.DOWN
+
+	# horizontal direction prioritized over vertical direction
+	if direction.x < 0:
+		entity.direction = Direction.LEFT
+	elif direction.x > 0:
+		entity.direction = Direction.RIGHT
 
 func enter(previous_state_path: String, data := {}) -> void:
 	pass
